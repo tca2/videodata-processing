54617a04e4893048ed906a466771cd2143a542f1
pnb <pnigelb@gmail.com>
2021-02-11 15:57:27 -0600

    Apply method to all keypoints

--- data_onemin/tracking_analysis.py
+++ data_onemin/tracking_analysis.py
@@ -31,7 +31,8 @@ def calculate_distances(keypoint_dataframe, keypoint_num):
                             index_closest = indexn
                     if closest_dist < 10:
                         break  # Close enough; stop looking further back
-            matches.at[row_i, ['closest_index', 'closest_dist', 'second_closest_dist']] = \
+            matches.at[row_i, [keypoint_num + '_closest_index', keypoint_num + '_closest_dist',
+                               keypoint_num + '_second_closest_dist']] = \
                 [index_closest, closest_dist, second_closest_dist]
         prev_frames.append(frame)
     return matches
@@ -41,12 +42,13 @@ fileslist = glob.glob('*.csv')
 print('Running script on these .csv files in current directory:\n', fileslist, '\n')
 for file in fileslist:
     df = pd.read_csv(file)
-    dict_of_keypoint_dfs = {f'df_{col[:-2]}': df[['frame_num', col, col.replace('x', 'y')]]
+    dict_of_keypoint_dfs = {col[:-2]: df[['frame_num', col, col.replace('x', 'y')]]
                             for col in df.columns if re.search(r'^keypoint\d*_x', col)}
     # TODO: We should probably filter the keypoints and only try to match on ones with few 0s
     # (OpenPose detection failures) to save a bit of computational power.
+    result_df = pd.DataFrame(index=df.index)
     for key, value in dict_of_keypoint_dfs.items():
-        print(key)
         dist_df = calculate_distances(value, key)
-        dist_df.to_csv(file + '-' + key + '.csv', index_label='orig_index')
-        break
+        # TODO: Change all the input/output filenames and parameters to argparse
+        result_df[dist_df.columns] = dist_df
+        result_df.to_csv(file + '-match_indices.csv', index_label='orig_index')
